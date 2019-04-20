315904638f8ccc27f755dfd334ee385145fddd20
Tal Levy <orntal17@gmail.com>
2019-04-20 17:06:28 +0300

    Removed redundant code

--- FlightSimulator/ViewModels/FlightBoardViewModel.cs
+++ FlightSimulator/ViewModels/FlightBoardViewModel.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using FlightSimulator.Model;
 using System.ComponentModel;
 
 namespace FlightSimulator.ViewModels
